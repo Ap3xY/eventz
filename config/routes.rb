de70803f371eb97ecfb49349d2f2a974db477fe8
Alex <alexvdberg010109@gmail.com>
2024-12-04 11:35:15 +0200

    feat(forms): added a editing route that takes the user to the editing screen of a single event and then uses a form to allow the user to edit the data

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   root "events#index"
   get "events" => "events#index"
   get "events/:id" => "events#show", as: "event"
+  get "events/:id/edit" => "events#edit", as: "edit_event"
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
