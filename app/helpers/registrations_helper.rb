b8831de1c8f41b25147bda6c24d1c2e101b635a3
Alex <alexvdberg010109@gmail.com>
2024-12-11 13:21:04 +0200

    feat(one-to-many: logic): learned how to add logic to the model to check if there are still spots open on an event

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,2 +1,9 @@
 module RegistrationsHelper
+  def register_or_sold_out(event)
+    if event.sold_out?
+      content_tag :span, "Sold Out!", class: "inline-block py-2 px-4 bg-gray-500 text-white rounded-md cursor-not-allowed"
+    else
+       link_to "Register!", new_event_registration_path(event), class: "inline-block py-2 px-4 bg-green-600 hover:bg-green-700 text-white rounded-md transition-colors duration-200"
+    end
+  end
 end
