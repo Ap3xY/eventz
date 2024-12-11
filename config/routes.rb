71986779c9f6ca5a15c26ea9b8a509ef707bf5be
Alex <alexvdberg010109@gmail.com>
2024-12-11 17:11:02 +0200

    feat(sign in): learned how to implement the initial ground work for the sign in section

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,6 +8,8 @@ Rails.application.routes.draw do
   resources :events do
     resources :registrations
   end
+
+  resource :session, only: [ :new, :create, :destroy ]
 
   resources :users
   get "signup" => "users#new"
